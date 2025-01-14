a0670a7238c000e2164be91ffcbe3560d8017d01
Santiago Cardenas <alexander.cardenas10@outlook.com>
2025-01-14 12:07:00 -0500

    Complete contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { BLUE } from "../constants/colors";
+import ContactForm from "./ContactForm";
 
 const ContactContainer = styled.section`
   font-family: Arial, Helvetica, sans-serif;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 64px;
+  margin-top: 40px;
 `;
-const Paragrahp = styled.p`
+const Paragraph = styled.p`
   text-align: center;
   font-weight: 700;
   font-size: 40px;
@@ -19,10 +20,11 @@ const Paragrahp = styled.p`
 const Contact = () => {
   return (
     <ContactContainer>
-      <Paragrahp>
-        I'm always interested in hearing about new projects, so if you'd like to
+      <Paragraph>
+        I&apos;m always interested in hearing about new projects, so if you&apos;d like to
         chat please get in touch.
-      </Paragrahp>
+      </Paragraph>
+      <ContactForm />
     </ContactContainer>
   );
 };
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import styled from "styled-components";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { GREEN, HOVERGREEN } from "../constants/colors";
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Input = styled.input`
+  margin-top: 12px;
+  margin-bottom: 24px;
+  border-radius: 4px;
+  padding: 15px;
+  width: 100%;
+  box-sizing: border-box;
+`;
+
+const TextArea = styled.textarea`
+   border-radius: 4px;
+   border: 2px solid ${GREEN};
+   height:100px;
+   width: 100%;
+   padding: 15px;
+   box-sizing: border-box;
+   margin-bottom: 24px;
+   margin-top: 12px;
+`
+
+const Submit = styled.input`
+  border: none;
+  border-radius: 4px;
+  padding: 6px 8px 6px 8px;
+  color: white;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 1.5;
+  letter-spacing: 0.00938em;
+  text-transform: none;
+  background-color: ${GREEN};
+  margin-top: 24px;
+  margin-bottom: 24px;
+  color: "white";
+  &:hover {
+    background-color: ${HOVERGREEN};
+  }
+`;
+
+const Paragraph = styled.p`
+  color: red;
+  font-size: 14px;
+  margin-top: -10px;
+  margin-bottom: 0px;
+  width: 100%;
+  text-align: left;
+`
+
+const schema = yup
+  .object({
+    Name: yup.string().required(),
+    Email: yup.string().email().required(),
+    Message: yup.string().required(),
+  })
+  .required();
+
+const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Input {...register("Name")} />
+      <Paragraph>{errors.Name?.message}</Paragraph>
+      <Input {...register("Email")} />
+      <Paragraph>{errors.Email?.message}</Paragraph>
+      <TextArea {...register("Message")} />
+      <Paragraph>{errors.Message?.message}</Paragraph>
+      <Submit type="submit" value="Get in Touch" />
+    </Form>
+  );
+};
+
+export default ContactForm;
