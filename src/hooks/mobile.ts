50097eb19bc375273bf808bbb900648ffda2aabf
Santiago Cardenas <alexander.cardenas10@outlook.com>
2025-01-15 11:53:45 -0500

    Add Mobile hook

---
+++ src/hooks/mobile.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  useEffect(() => {
+    const updateIsMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    updateIsMobile();
+    window.addEventListener("resize", updateIsMobile);
+    return () => window.removeEventListener("resize", updateIsMobile);
+  }, [innerWidth]);
+
+  return { isMobile, setIsMobile };
+};
+
+export default useIsMobile
